58d43d1e4e9c1e7bcfd3f44078cfc822b52dd5bf
Barry <barry.teoh@perceptionz.net>
2016-01-23 12:01:34 +0800

    Add in mongo DB Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,26 @@
-class User < ActiveRecord::Base
+class User
+  include Mongoid::Document
+  store_in collection: "users"
+
+  field :name, type: String
+  field :email, type: String
+  field :facebook_token, type: String
+
+  # Types of fields
+  # Array
+  # BigDecimal
+  # Boolean
+  # Date
+  # DateTime
+  # Float
+  # Hash
+  # Integer
+  # BSON::ObjectId
+  # BSON::Binary
+  # Range
+  # Regexp
+  # String
+  # Symbol
+  # Time
+  # TimeWithZone
 end
