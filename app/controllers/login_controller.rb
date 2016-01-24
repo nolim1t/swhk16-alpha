fb334f95ebe026bb774fda1c267f4d65b168ed56
Barry <barry.teoh@perceptionz.net>
2016-01-24 12:56:58 +0800

    Signup

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,14 +1,15 @@
 class LoginController < ActionController::Base
+  layout 'unauthenticated_header'
   def login
     username = params['username']
     password = params['password']
     find_user = User.find(:all, :conditions => {:username => username, :password => password})
     logger.info("#{find_user}")
-    
+
     render :text => "Hi"
   end
 
   def logup
-  	
+
   end
 end
