a314bf386985d3f2e98ccc1732af4fa1280ac7dd
Barry <barry.teoh@perceptionz.net>
2016-01-24 11:50:49 +0800

    Add card detail page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get '/cards/index' => 'cards#index', as: :cards_index
-
+  get '/cards/detail' => 'carddetail#index', as: :carddetail_index
+  
   match '/login' => 'login#login', :via => [:post]
 end
