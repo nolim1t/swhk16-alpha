cb7163a276f42cb7e6f045d175766cebc0ae8f2c
Barry <barry.teoh@perceptionz.net>
2016-01-24 11:06:07 +0800

    home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,5 +10,6 @@ class HomeController < ActionController::Base
     password = params['password']
     find_user = User.find(:all, :conditions => {:username => username, :password => password})
     logger.info("#{find_user}")
+    render :text => "Hi"
   end
 end
