cbcae83165fe8faa542eb37e57a51e06b47fc841
Barry <barry.teoh@perceptionz.net>
2016-01-24 11:22:22 +0800

    Resolve conflicts. and fix up my changes.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,7 @@ class LoginController < ActionController::Base
     password = params['password']
     find_user = User.find(:all, :conditions => {:username => username, :password => password})
     logger.info("#{find_user}")
+    
     render :text => "Hi"
   end
 end
