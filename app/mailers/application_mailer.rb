c3e98fd8aeb772eccfe68e9e7c15863ceb99718f
Barry <barry.teoh@perceptionz.net>
2016-01-31 22:51:22 +0800

    User must confirm email

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "info@vaultron.co"
+  layout 'mailer'
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,5 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = {host: "www.vaultron.co"}
 end
