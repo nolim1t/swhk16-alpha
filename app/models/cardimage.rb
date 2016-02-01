7a587d3f1fced7101ae13ee97e2517c09469b8e4
Barry <barry.teoh@perceptionz.net>
2016-02-01 17:46:55 +0800

    Remove more broken code

--- app/models/cardimage.rb
+++ app/models/cardimage.rb
@@ -1,12 +1,10 @@
 class Cardimage
   include Mongoid::Document
   store_in collection: "cardimages"
-  attr_accessible :image
 
   field :url, type: String
   field :create_date, type: Date
 
   field :card_id, type: BSON::ObjectId
 
-  mount_uploader :avatar, CarduploaderUploader
 end
