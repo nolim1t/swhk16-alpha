ea0928a1ae4640c41e85d7cdc1d65e1f826b566c
SophieZheng <bmzheng@gmail.com>
2016-02-01 17:08:57 +0800

    style navbar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,12 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( landing.css )
-Rails.application.config.assets.precompile += %w( Logo.png )
-Rails.application.config.assets.precompile += %w( front.jpg )
-Rails.application.config.assets.precompile += %w( back.jpg )
-Rails.application.config.assets.precompile += %w( front_1.jpg )
-Rails.application.config.assets.precompile += %w( back_1.jpg )
-Rails.application.config.assets.precompile += %w( background_copy.jpg )
+Rails.application.config.assets.precompile += %w( logolight.png )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
