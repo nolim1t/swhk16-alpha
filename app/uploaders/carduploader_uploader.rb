4d40224773d46c6891a3b0f1001f70f36316818c
Barry <barry.teoh@perceptionz.net>
2016-02-02 16:21:38 +0800

    Fix up links. Change pathname a little bit

--- app/uploaders/carduploader_uploader.rb
+++ app/uploaders/carduploader_uploader.rb
@@ -12,7 +12,7 @@ class CarduploaderUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{ENV['RAILS_ENV']}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -33,6 +33,7 @@ class CarduploaderUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
      process :resize_to_fit => [400, 400]
+     process convert: 'png'
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
