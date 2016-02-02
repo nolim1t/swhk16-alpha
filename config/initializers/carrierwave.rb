dbda29e1ad200f64cc1d5534147dfb2594387ddb
Barry <barry.teoh@perceptionz.net>
2016-02-02 14:45:14 +0800

    Trigger another deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
   config.cache_dir = 'carrierwave'
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                        # required
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],                        # required
-    region:                ENV['AWS_REGION'] || 'us-east-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.amazonaws.com',             # optional, defaults to nil
-    endpoint:              'https://s3.amazonaws.com/' # optional, defaults to nil
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region:                ENV['AWS_REGION'] || 'us-east-1',
+    host:                  's3.amazonaws.com',
+    endpoint:              'https://s3.amazonaws.com/'
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME'] || 'vaultron-beta-dev'                          # required
   config.fog_public     = true                                      # optional, defaults to true
