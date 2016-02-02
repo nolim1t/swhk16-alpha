9b7aab775506947baf7e61931d4a615926f10215
Barry <barry.teoh@perceptionz.net>
2016-02-02 17:26:03 +0800

    Placeholder image

--- app/uploaders/carduploader_uploader.rb
+++ app/uploaders/carduploader_uploader.rb
@@ -16,12 +16,13 @@ class CarduploaderUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    "/placeholder.png"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [400, 300]
