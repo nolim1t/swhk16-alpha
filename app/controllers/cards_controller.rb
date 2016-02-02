14b60f791352d9a047df7fa48e71fc470fda91a3
Barry <barry.teoh@perceptionz.net>
2016-02-02 11:28:13 +0800

    Work towards issue #3 - Add in template changes and some form UX functionality

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,12 +9,30 @@ class CardsController < ApplicationController
 	# POST /cards/new
 	def newform
 		puts "new card"
-		puts params[:cards].inspect
-		redirect_to :cards_index
+		if params[:cards]["name"] != "" and params[:cards]["game"] != "" and params[:cards]["collection"] != "" then
+			if params[:cards]["Upload Picture"] then
+				puts params[:cards].inspect
+				redirect_to :cards_index
+			else
+				puts "Need a picture"
+				@errormsg = "A card requires a photo"
+				@prefilled_name = params[:cards]["name"]
+				@prefilled_game = params[:cards]["game"]
+				@prefilled_collection = params[:cards]["collection"]
+				render :template => "cards/new"
+			end
+		else
+			puts "Invalid parameters"
+			@errormsg = "Please complete the entire form"
+			render :template => "cards/new"
+		end
 	end
 	# GET /cards/new
 	def new
-
+		@prefilled_name = ""
+		@prefilled_game = ""
+		@prefilled_collection = ""
+		render :template => "cards/new"
 	end
 
 	def transfer
