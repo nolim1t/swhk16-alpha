98eb04ac39fe6d85bba40bf1d51213b1c70bec33
Barry <barry.teoh@perceptionz.net>
2016-02-10 21:13:18 +0800

    #27 - Allow vendors to see other cards in the system so that they can approve or reject them. However they can't approve or reject their own cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -119,7 +119,7 @@ class CardsController < ApplicationController
 				@cardimages << card_2
 			}
 			# Check if owner matches the database
-			if @card.owner_id == current_user._id.to_s then
+			if (@card.owner_id == current_user._id.to_s) or (current_user.accounttype == "vendor") then
 				@method = env['REQUEST_METHOD']
 				if current_user.timezone == '' or current_user.timezone == nil then
 					@timezone = current_user.timezone
