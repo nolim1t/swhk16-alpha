985b5dc52552c14e8c8567be28f7472115e7d467
Barry <barry.teoh@perceptionz.net>
2016-02-11 12:36:24 +0800

    Flesh out the transfer controller for #5

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -3,19 +3,26 @@ class TransferController < ActionController::Base
   def outbound
     if params[:transfer] then
       card = Card.find(params[:transfer][:cardid])
-      puts "#{card[:cardname]}"
       if card[:cardname] then
         if card[:cardname].downcase == params[:transfer][:cardname].downcase then
-          if params[:transfer][:agree] == "1" then
-            puts "Transfer request: Card ID=#{params[:transfer][:cardid]} / Name of card: #{params[:transfer][:cardname]}/ To: #{params[:transfer][:email]} / Agreed: #{params[:transfer][:agree]}"
+          if card[:transfer_status] != 1 then
+            if params[:transfer][:agree] == "1" then
+              puts "Transfer request: Card ID=#{params[:transfer][:cardid]} / Name of card: #{params[:transfer][:cardname]}/ To: #{params[:transfer][:email]} / Agreed: #{params[:transfer][:agree]}"
+              # TODO: Set up another ruby helper to facilitate this transfer
+            else
+              flash[:error] = "You must accept the terms before you can transfer this card"
+              self.goback
+            end
           else
-            puts "User didn't accept agreement"
+            flash[:error] = "Card is currently being transferred"
           end
         else
-          puts "User didn't type the name of the card in correctly"
-        end        
+          flash[:error] = "You must type in the name of the card before you can transfer it"
+          self.goback
+        end
       else
-        puts "Card not found"
+        flash[:error] = "Unable to locate the card that you were trying to transfer"
+        self.goback
       end
     else
       puts "Invalid parameters"
@@ -23,5 +30,14 @@ class TransferController < ActionController::Base
   end
 
   def inbound
+  end
+
+  # Helpers
+  def goback
+    if request.referer then
+      redirect_to request.referer
+    else
+      redirect_to "/"
+    end
   end
 end
