a0a3d9c242bf61f188f968f60ea4d6a746be4962
Barry <barry.teoh@perceptionz.net>
2016-02-11 13:15:24 +0800

    Add some more transfer rules

--- app/helpers/transfer_helper.rb
+++ app/helpers/transfer_helper.rb
@@ -3,8 +3,26 @@ module TransferHelper
     def self.send(from, to, assetid, assettype)
       result = {:info => "", :error => ""}
       if assettype == "card" then
-        result[:info] = "Sending Asset #{assetid} to #{to} (originator: #{from})"
-        result
+        recipient = User.where(email: to)
+        if recipient.count == 1 then
+          puts "Recipient: #{recipient[0][:email]}"
+          begin
+            card = Card.find(assetid)
+          rescue
+            card = nil
+          end
+          if card != nil
+            puts "#{card}"
+            result[:info] = "Sending Asset #{assetid} to #{to} (originator: #{from})"
+            result
+          else
+            result[:error] = "Card doesn't exist"
+            result
+          end
+        else
+          result[:error] = "The user you tried to transfer to doesn't exist. But don't worry, very soon they will get an invite code and you won't see this message"
+          result
+        end
       else
         result[:error] = "Invalid asset type"
         result
