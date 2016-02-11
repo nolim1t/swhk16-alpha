db3415b6bf9a9e5ce9bfe8f222883bd6663bfe4d
Barry <barry.teoh@perceptionz.net>
2016-02-11 10:49:22 +0800

    Create model for issue #5

---
+++ app/models/transfer.rb
@@ -0,0 +1,11 @@
+class Assettransfer
+  include Mongoid::Document
+  store_in collection: "assettransfers"
+
+  field :asset_id, type: String # This is the representation of a card_id
+  field :asset_type, type: String, default: "card" # For now, card
+  field :sender_email, type: String # Who is sending
+  field :receiver_email, type: String # Who is receiving
+  field :arbiter_email, type: String, default: "info@vaultron.co" # Who is the middleman
+  field :create_date, type: Date
+end
