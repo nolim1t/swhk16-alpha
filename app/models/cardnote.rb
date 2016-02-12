adfb644c55c7c73c0b9aab03a13e7d0e51529550
Barry <barry.teoh@perceptionz.net>
2016-02-12 15:58:41 +0800

    Change all models to datetime. And change the detail page to show the time

--- app/models/cardnote.rb
+++ app/models/cardnote.rb
@@ -2,7 +2,7 @@ class Cardnote
   include Mongoid::Document
   store_in collection: "cardnotes"
   field :text, type: String
-  field :create_date, type: DateTime
+  field :create_date, type: DateTime, default: ->{ Time.new }
 
   field :card_id, type: String
 end
--- app/models/invitecode.rb
+++ app/models/invitecode.rb
@@ -1,9 +1,10 @@
+# Invitecode.create() will just make a random code
 class Invitecode
   include Mongoid::Document
   store_in collection: "invitecodes"
 
-  field :code, type: String
-  field :create_date, type: Date
+  field :code, type: String, default: -> {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[rand(26), 10] + 'AEIOU'[rand(5), 10]}
+  field :create_date, type: DateTime, default: ->{ Time.new }
 
   field :postprocess_instructions, type: String
 end
