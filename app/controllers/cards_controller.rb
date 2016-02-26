ea318710faec21abe1a58a7fbf352975fe979b2b
Barry <barry.teoh@perceptionz.net>
2016-02-26 12:21:10 +0800

    Set up the ability for expert users to validate cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -141,7 +141,7 @@ class CardsController < ApplicationController
 				@cardimages << card_2
 			}
 			# Check if owner matches the database
-			if (@card.owner_id == current_user._id.to_s) or (current_user.accounttype == "vendor" or current_user.accounttype == "admin") then
+			if (@card.owner_id == current_user._id.to_s) or ((current_user.accounttype == "vendor" and current_user.identity_verified == 1) or (current_user.accounttype == "expert") or (current_user.accounttype == "admin")) then
 				@method = env['REQUEST_METHOD']
 				if current_user.timezone == '' or current_user.timezone == nil then
 					@timezone = current_user.timezone
