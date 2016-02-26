b98bd3c8899b825090dfb77d627b05d6f4e9f407
Barry <barry.teoh@perceptionz.net>
2016-02-26 11:55:59 +0800

    expert user UI changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User
   field :timezone, type: String
   field :identity_verified, type: Integer # 1 if user identity is verified (for later implementations)
   field :create_date, type: Date # For working out when the next billing day is
-  field :accounttype, type: String, default: "standard" # What we signed up as
+  field :accounttype, type: String, default: "standard" # What we signed up as (accepted values is: 'standard', 'expert', 'vendor', 'admin')
   field :billing_plan, type: Integer, default: 0 # Right now: 0 = limited plan, 1 = super early adopter, 2 = early adopter, 3 = standard
   # Types of fields
   # Array
