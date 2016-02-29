c45ba997854adb007f05c6c6760629a038c70e91
Barry <barry.teoh@perceptionz.net>
2016-02-29 18:22:43 +0800

    Show experts page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ActionController::Base
  	layout 'application'
- 	
+
 	def index
 
 	end
 
   def new
+  end
+
+  def showexperts
+    @experts = User.where(:expert_contact_info => {:$exists => true}, :expert_location_info => {:$exists => true}, :coordinates => {:$exists => true})
   end
 end
