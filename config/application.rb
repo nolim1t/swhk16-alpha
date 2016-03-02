bdcf896c71e2f2975059eb7dbda931fae3e85d8d
Barry <barry.teoh@perceptionz.net>
2016-03-02 12:20:47 +0800

    Available locales

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Vaultron
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = %i[en zh]
     config.i18n.default_locale = :en
   end
 end
