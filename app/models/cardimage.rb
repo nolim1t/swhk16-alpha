cbf6a2cdb696afd1d54911fcb792bbce7ece3002
Barry <barry.teoh@perceptionz.net>
2016-03-03 10:06:09 +0800

    #100 - update as per http://stackoverflow.com/questions/32292528/carrierwave-files-not-deleting-from-s3

--- app/models/cardimage.rb
+++ app/models/cardimage.rb
@@ -7,4 +7,10 @@ class Cardimage
   field :image_type, type: String # Enforce in app as 'front' or 'back'
   field :image_note, type: String
   mount_uploader :photo, CarduploaderUploader
+  before_destroy :destroy_assets
+
+  def destroy_assets
+    self.photo.remove! if self.photo
+    self.save!
+  end
 end
