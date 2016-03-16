3c34e807d4b09cd82f07b72d0486336d9c30ca3a
Barry <barry.teoh@perceptionz.net>
2016-03-16 11:57:37 +0800

    Try to remove jquery

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,11 +1,13 @@
-// $(document).ready(function(){
-// 	var dismiss_popover = function(selector){
-// 		$(selector).click();
-// 	}
+/*
+ $(document).ready(function(){
+ 	var dismiss_popover = function(selector){
+ 		$(selector).click();
+ 	}
 
-// 	$('[data-toggle="popover"]').popover({ html: true });
-// }
+ 	$('[data-toggle="popover"]').popover({ html: true });
+ }
+ */
+
 $(function () {
   $('[data-toggle="popover"]').popover({ html: true })
 })
-
