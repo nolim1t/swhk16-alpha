6a8e3fd038f523e208fbf697cd0b2c45dd4382de
Nicolas Bouyt <nicolasbouyt@Nicolass-MacBook-Air.local>
2016-03-23 17:34:34 +0800

    testing new images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,10 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( landing.css )
 Rails.application.config.assets.precompile += %w( logolight.png )
-Rails.application.config.assets.precompile += %w( dog.png )
+Rails.application.config.assets.precompile += %w( landing1.png )
+Rails.application.config.assets.precompile += %w( landing2.png )
+Rails.application.config.assets.precompile += %w( landing3.png )
+Rails.application.config.assets.precompile += %w( landing4.png )
 Rails.application.config.assets.precompile += %w( intro-bg.png )
 
 # Add additional assets to the asset load path
